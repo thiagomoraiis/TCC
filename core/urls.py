2929441ca9d25745c871bd3f766fd1c1b981c4a4
thiagomoraiis <thiagomorais2605@gmail.com>
2023-08-05 18:13:30 -0300

    feat: criando pagina do simulador de nota

--- core/urls.py
+++ core/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import login, index, presentation, detail_news
+from .views import login, index, presentation, detail_news, register, simulator
 
 app_name = 'core'
 
@@ -7,5 +7,7 @@ urlpatterns = [
     path('', index, name="index"),
     path('presentation', presentation, name='presentation'),
     path('login/', login, name="login"),
-    path('detail/', detail_news, name="detail")
+    path('detail/', detail_news, name="detail"),
+    path('register/', register, name="register"),
+    path('simulator/', simulator, name="simulator"),
 ]
--- core/views.py
+++ core/views.py
@@ -4,11 +4,22 @@ from django.shortcuts import render
 def index(request):
     return render(request, 'core/pages/index.html')
 
+
 def presentation(request):
     return render(request, 'core/pages/presentation.html')
+
 
 def login(request):
     return render(request, 'student/pages/form_login.html')
 
+
 def detail_news(request):
     return render(request, 'core/pages/detail_news.html')
+
+
+def register(request):
+    return render(request, 'student/pages/form_register.html')
+
+
+def simulator(request):
+    return render(request, 'core/pages/simulator.html')
