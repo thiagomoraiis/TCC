cf9c8902490b3a5a0aa7787a2ea5a5801b040983
thiagomoraiis <thiagomorais2605@gmail.com>
2023-11-19 17:04:20 -0300

    resolvendo problema da imagem no card de noticias

--- core/views.py
+++ core/views.py
@@ -1,11 +1,38 @@
-from django.views.generic import (TemplateView, CreateView)
+from typing import Any
 from tip.forms import TipsModelForm
 from django.urls import reverse_lazy
 from tip.models import Tip
+from event.models import Event
+from django.views.generic import (
+    TemplateView, CreateView, ListView, DetailView
+)
 
 
-class IndexListView(TemplateView):  # trocar para ListView
+class IndexListView(ListView):
     template_name = 'core/pages/index.html'
+    model = Event
+    # context_object_name = 'event'
+    queryset = Event.objects.all()
+
+    def get_context_data(self, **kwargs: Any):
+        context = super().get_context_data(**kwargs)
+        import re
+
+        image_urls = []
+
+        posts = self.get_queryset()
+
+        for post in posts:
+            content = post.content
+
+            matches = re.findall(r'<img .*?src="(.*?)".*?>', content)
+
+            if matches:
+                image_urls.extend(matches)
+
+        context['image_urls'] = image_urls
+        context['event'] = posts
+        return context
 
 
 class PresentationTemplateView(TemplateView):
@@ -32,5 +59,8 @@ class CreatePost(CreateView):
         return super().form_valid(form)
 
 
-class DetailPost(TemplateView):
+class DetailPost(DetailView):
     template_name = 'core/pages/detail_news.html'
+    queryset = Event.objects.all()[:4]
+    context_object_name = 'event'
+    pk_url_kwarg = 'id'
