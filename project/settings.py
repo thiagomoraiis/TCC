dc912222212969d267c0a812a763e1979112bcfb
Thiago Morais Silva <morais.t@academico.ifrn.edu.br>
2023-11-24 10:21:32 -0300

    iniciando configuração do Docker

--- project/settings.py
+++ project/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-qh@f(az#05rm^2t=c#1e*u$xy5!7f&!l3$#er(t-h_+vf5_%*5'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
@@ -92,9 +92,17 @@ WSGI_APPLICATION = 'project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'iforum',
+        'USERNAME': 'postgres',
+        'PASSWORD': '123456',
+        'HOST': 'postgres',
+        'PORT': '5432'
     }
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': BASE_DIR / 'db.sqlite3',
+    # }
 }
 
 
